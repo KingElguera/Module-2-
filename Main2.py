013278f39f65d279fb6554a309c3e970232ec7e5
KingElguera <elgueraishimwe@gmail.com>
2025-10-02 17:22:59 +0000

    code2emeversion

---
+++ Main2.py
@@ -0,0 +1,68 @@
+# TODO: Create a list of your favorite fruits
+
+# TODO: Add a new fruit to the end of the list
+
+# TODO: Remove the second fruit from the list
+
+# TODO: Sort the list alphabetically
+
+# TODO: Create a new list with the first three fruits
+
+# Print the original list and the new list
+
+# Exercice 1 module 2
+
+# fruits = ["pomme", "bananne", "mangue"]
+# fruits.append("fraise")
+# fruits.sort()
+# print(fruits)
+# new_list = [3]
+# print("new list", fruits)
+
+# TODO: Create a tuple with your favorite colors
+
+# TODO: Try to modify one of the colors (this should raise an error)
+
+# TODO: Count how many times a specific color appears in the tuple
+
+# TODO: Find the index of a specific color
+
+# TODO: Create a new tuple by concatenating two existing tuples
+
+# Print the results of each operation
+
+#Exercice 2
+# colors = ("bleu", "bleu", "noire", "vert", "rouge", "noire")
+# colors1 = ("mauve", "gris", "turquoise")
+# all_colors = colors + colors1 
+# print(all_colors)
+
+
+# TODO: Create a dictionary representing a person (name, age, city)
+
+# TODO: Add a new key-value pair for the person's occupation
+
+# TODO: Update the person's age
+
+# TODO: Remove the 'city' key-value pair
+
+# TODO: Print all keys, then all values
+
+# TODO: Check if a specific key exists in the dictionary
+
+# Print the final dictionary
+
+# Exercice 3 
+
+
+
+
+
+
+
+
+
+
+
+
+
