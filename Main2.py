278cb1777d40aeafe354633d25d717865a991c4f
KingElguera <elgueraishimwe@gmail.com>
2025-10-03 17:43:54 +0000

    la suite

--- Main2.py
+++ Main2.py
@@ -39,12 +39,31 @@
 
 
 # TODO: Create a dictionary representing a person (name, age, city)
+# personne = { 
+#     "name": "kingelguera",
+#     "age":  23,
+#     "city": "clermont ferrand"
+# }
+# print(f'dictionnaire de départ,{personne}')
 
 # TODO: Add a new key-value pair for the person's occupation
+# personne["occupation"] = "étudiant"
+# print(f'Après ajout de la profession', personne)
 
 # TODO: Update the person's age
+# Individu = { 
+#     "name": "lucascurry",
+#     "age": 20,
+#     "city": "ottawa",
+#     "occupation": "droguer"
+# }
+# print(f'avant,{Individu}')
+# Individu["age"] = 25
+# print(f'après,{Individu}') 
 
 # TODO: Remove the 'city' key-value pair
+# del Individu["city"]
+# print(f'après,{Individu}')
 
 # TODO: Print all keys, then all values
 
@@ -53,6 +72,69 @@
 # Print the final dictionary
 
 # Exercice 3 
+
+# personne = { 
+#     "name": "kingelguera",
+#     "age":  23,
+#     "city": "clermont ferrand"
+# }
+# print(f'dictionnaire de départ,{personne}')
+ 
+# personne["occupation"] = "étudiant"
+# print(f'Après ajout de la profession', personne)
+
+# Individu = { 
+#     "name": "lucascurry",
+#     "age": 20,
+#     "city": "ottawa",
+#     "occupation": "droguer"
+# }
+# print(f'avant,{Individu}')
+# Individu["age"] = 25
+# print(f'après,{Individu}') 
+# del Individu["city"]
+# print(f'après,{Individu}')
+# print(f'Maura,{Individu}')
+# print(f'les clés du dictionnaire:, {Individu.keys}')
+# print(f'les valeurs du dictionnaire:, {Individu.values}')
+# if "age" in Individu: 
+#     print("la clé 'age' existe dans le dictionnaire")
+# else:
+#     print("la clé 'age' n'existe pas ")
+
+
+# Exercice 4
+
+# # TODO: Create two sets of numbers
+# set1 = {1, 2, 3, 4}
+# set2 = {3, 4, 5, 6, 4}
+# print("set1", set1)
+# print("set2", set2)
+# # TODO: Find the union of the two sets
+# union_set = set1.union(set2)
+# print("union des deux sets", union_set)
+
+# # TODO: Find the intersection of the two sets
+# intersection_set = set1.intersection(set2)
+# print("intersection(elements communs): ", intersection_set)
+# # TODO: Find the difference between the first and second set
+# difference_set = set1.difference(set2)
+# print("différence(élements dans set1 mais pas dans set2): ", difference_set)
+# # TODO: Add a new element to one of the sets
+# set1.add(5)
+# print(set1)
+# # TODO: Remove an element from one of the sets
+# set2.remove(6)
+# print(set2)
+# # Print the results of each operation
+
+
+
+
+
+
+
+
 
 
 
